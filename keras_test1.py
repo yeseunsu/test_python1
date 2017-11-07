c319a75db4f806dc56dfab0d057c71b27f85a322
yeseunsu <2919yesdmstndi1>
2017-11-07 11:45:13 +0900

    some files added

---
+++ keras_test1.py
@@ -0,0 +1,57 @@
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation
+#
+# model = Sequential()
+# model.add(Dense(32, activation='relu', input_dim=100))
+# model.add(Dense(1, activation='sigmoid'))
+# model.compile(optimizer='rmsprop',
+#               loss='binary_crossentropy',
+#               metrics=['accuracy'])
+#
+# # Generate dummy data
+# import numpy as np
+# data = np.random.random((1000, 100))
+# labels = np.random.randint(2, size=(1000, 1))
+#
+# # Train the model, iterating on the data in batches of 32 samples
+# model.fit(data, labels, epochs=10, batch_size=32)
+# score = model.evaluate(data, labels, batch_size=32, verbose=1, sample_weight=None)
+#
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+import numpy as np
+
+
+
+x_train = np.random.random((1000, 50))
+y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+x_test = np.random.random((100, 50))
+y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+model = Sequential()
+# Dense(64) is a fully-connected layer with 64 hidden units.
+# in the first layer, you must specify the expected input data shape:
+# here, 20-dimensional vectors.
+model.add(Dense(64, activation='relu', input_dim=50))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          epochs=20,
+          batch_size=128)
+score = model.evaluate(x_test, y_test, batch_size=128)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
