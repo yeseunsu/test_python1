60e9d177b8fa9984c38d7caa5887e739e3cde187
yeseunsu <2919yesdmstndi1>
2017-11-10 10:46:48 +0900

    time check

--- mnist_cnn_keras.py
+++ mnist_cnn_keras.py
@@ -11,10 +11,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+import time
 
 batch_size = 128
 num_classes = 10
-epochs = 12
+epochs = 24
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -59,11 +60,15 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
+start_time = time.time()
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
+print("Training Time: --- %s seconds ---" % (time.time() - start_time))
+
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
